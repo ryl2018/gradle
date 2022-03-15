eda4c54ecd6403974c715e6de569ababb261df31
Bálint Hegyi <bhegyi@gradle.com>
2022-03-15 14:56:43 +0100

    Revert "Deprecate `useLegacyGpg`"
    This reverts commit cdf4ac43

--- subprojects/security/src/main/java/org/gradle/security/internal/gnupg/GnupgSettings.java
+++ subprojects/security/src/main/java/org/gradle/security/internal/gnupg/GnupgSettings.java
@@ -52,17 +52,10 @@ public class GnupgSettings {
         }
     }
 
-    /**
-     * @deprecated drives no more the GPG binary selection, use {@link #setExecutable(String)} directly to define the executable
-     */
     public void setUseLegacyGpg(boolean useLegacyGpg) {
         this.useLegacyGpg = useLegacyGpg;
     }
 
-    /**
-     * @deprecated not used anymore, see {@link #setUseLegacyGpg(boolean)}
-     */
-    @Deprecated()
     public boolean getUseLegacyGpg() {
         return useLegacyGpg;
     }
